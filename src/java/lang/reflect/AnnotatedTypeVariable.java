a25e0c1c69337cd042ca2ecdf9448eccf5682a9f
康建伟 <kangjianwei1990@qq.com>
2019-02-10 00:09:33 +0800

    :bulb: 【java.lang.reflect.AnnotatedTypeVariable】 【被注解的类型变量】

--- src/java/lang/reflect/AnnotatedTypeVariable.java
+++ src/java/lang/reflect/AnnotatedTypeVariable.java
@@ -32,8 +32,8 @@ package java.lang.reflect;
  *
  * @since 1.8
  */
+// 【被注解的类型变量】
 public interface AnnotatedTypeVariable extends AnnotatedType {
-
     /**
      * Returns the potentially annotated bounds of this type variable.
      * If no bound is explicitly declared, the bound is unannotated
@@ -42,6 +42,7 @@ public interface AnnotatedTypeVariable extends AnnotatedType {
      * @return the potentially annotated bounds of this type variable
      * @see TypeVariable#getBounds()
      */
+    // 类型变量的【被注解上界类型】
     AnnotatedType[] getAnnotatedBounds();
 
     /**
@@ -56,6 +57,7 @@ public interface AnnotatedTypeVariable extends AnnotatedType {
      *
      * @since 9
      */
+    // 实现类中返回null
     @Override
     AnnotatedType getAnnotatedOwnerType();
 }
